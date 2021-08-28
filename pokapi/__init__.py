bcaf5fcaa0a61034ae610dabaf19a07410ec649a
mhucka <mhucka@caltech.edu>
2021-08-27 19:18:20 -0700

    Add __init__.py

---
+++ pokapi/__init__.py
@@ -0,0 +1,48 @@
+'''
+Pokapi: Python Okapi Interface
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2021 by the California Institute of Technology.  This code
+is open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+# Package metadata ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#  ╭────────────────────── Notice ── Notice ── Notice ─────────────────────╮
+#  |    The following values are automatically updated at every release    |
+#  |    by the Makefile. Manual changes to these values will be lost.      |
+#  ╰────────────────────── Notice ── Notice ── Notice ─────────────────────╯
+
+__version__     = '0.0.0'
+__description__ = 'Python package for getting basic data from a FOLIO ILS server'
+__url__         = 'https://github.com/caltechlibrary/pokapi'
+__author__      = 'Michael Hucka'
+__email__       = 'mhucka@caltech.edu'
+__license__     = 'BSD 3-clause'
+
+
+# Exports.
+# .............................................................................
+
+from .exceptions import FolioError, DataMismatchError, NotFound
+from .folio      import Folio, FolioItem, FolioRecord
+
+__all__ = [Folio, FolioItem, FolioRecord, FolioError, DataMismatchError, NotFound]
+
+
+# Miscellaneous utilities.
+# .............................................................................
+
+def print_version():
+    print(f'{__name__} version {__version__}')
+    print(f'Authors: {__author__}')
+    print(f'URL: {__url__}')
+    print(f'License: {__license__}')
